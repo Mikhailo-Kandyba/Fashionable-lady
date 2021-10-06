f3934731dadf1107c7d085c717b25d094c682190
Mikhailo-Kandyba <mikhailo.kandyba0gmail.com>
2021-10-06 20:38:34 +0300

    goods and blog

--- src/js/main.js
+++ src/js/main.js
@@ -11,3 +11,42 @@ $('.menu-navigation-wrap a').click(function(){
     $(".menu-navigation-wrap").removeClass('show-menu');
     $("body").removeClass('overflow-hidden');
 });
+
+$( function() {
+    $( "#tabs" ).tabs();
+} );
+
+$('.slider-blog').slick({
+    dots: true,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
